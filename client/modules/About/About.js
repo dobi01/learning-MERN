0df55f3db0fe05434ceea56600cb89f49e3d7633
Dobi Okrasa <dobi.okrasa@gmail.com>
2018-04-01 14:22:23 +0200

    Add 'About' module

---
+++ client/modules/About/About.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './About.css';
+
+const About = () => {
+  return (
+    <div>
+      <h2>About</h2>
+      <p>Lorem ipsum dolor sit amet, eum ei affert persequeris.
+        Ut autem admodum est, eu pri audire virtute scripserit.
+        Nihil iudico detraxit nec in, an decore facilis vis.
+        Commodo senserit voluptatum ei eos. Vivendum principes ad vim, et vix harum vocibus.
+        No brute persius patrioque usu, te vis nulla ludus primis. Pri invenire volutpat petentium cu,
+        antiopam intellegam interpretaris ea sea. Soleat soluta in mel, et vitae fabulas adipisci has,
+        pri labores volutpat laboramus at. Pri ex euripidis accommodare. Vis unum oportere no.
+      </p>
+    </div>
+  );
+};
+
+export default About;
